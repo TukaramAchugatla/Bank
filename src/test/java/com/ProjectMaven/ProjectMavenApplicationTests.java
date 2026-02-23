b6beb15dda219a7b7b4f93c3a5c811cd568b1118
TukaramAchugatla <achugatlatukaram@gmail.com>
2026-02-23 21:27:48 +0530

    This is Java Project

---
+++ src/test/java/com/ProjectMaven/ProjectMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
